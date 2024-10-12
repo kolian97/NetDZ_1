775d2a849df924d8bffe4c6a4ef2af386c4e2318
kolian97 <kolian_97@mail.ru>
2024-10-12 13:49:13 +0300

    first commit

---
+++ NetDZ_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+namespace NetDZ_1
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            bool isNumeric1 = int.TryParse(args[0], out n);
+            bool isNumeric2 = int.TryParse(args[2], out n);
+            if (args.Length == 3 & isNumeric1 && isNumeric2)
+            {
+                int number1 = int.Parse(args[0]);
+                int number2 = int.Parse(args[2]);
+                if (args[1] == "+")
+                    Console.WriteLine(number1 + number2);
+                else if (args[1] == "-")
+                    Console.WriteLine(number1 - number2);
+                else if (args[1] == "*")
+                    Console.WriteLine(number1 * number2);
+                else if (args[1] == "/")
+                    Console.WriteLine(number1 / number2);
+                else Console.WriteLine("Не верная операция");
+            }
+            else Console.WriteLine("Не верное количество аргументов или неверный формат ввода");
+            return;
+        }
+    }
+}
